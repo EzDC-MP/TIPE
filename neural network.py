28af170d4c77766111699f724abaad9890475a6e
pedroellion <peperonieggwp@gmail.com>
2021-04-03 15:38:07 +0200

    NICE

--- neural network.py
+++ neural network.py
@@ -13,11 +13,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.model_selection import GridSearchCV
 
+### décale un matrice de données indéxées par un date à n jours en avance
+def timeshift_day(data,n):
+    data.index = data.index.shift(periods = n, freq ='D')
 
 
 tscv = TimeSeriesSplit(n_splits=5)
 
-covid = pd.read_excel('chiffre_covid_14032021.xlsx',index_col='date',parse_dates=True)#,index_col='date',parse_dates=True)
+covid = pd.read_excel('chiffre_covid_14032021 copie.xlsx',index_col='date',parse_dates=True)#,index_col='date',parse_dates=True)
 
 covid = covid[covid['granularite']=='pays']
 
@@ -25,10 +28,13 @@ covid = covid[covid['granularite']=='pays']
 for i in covid:
     if not (covid[i].name in ["deces",'cas_confirmes','reanimation','hospitalises']):
         covid.drop([i], axis=1, inplace = True)
-covid=covid[5:]
+
 
 covid = covid.dropna(axis=0)
+timeshift_day(covid['deces'],-7)
+covid=covid[7:]
 
+full_size=covid.count()[0]
 subject=['hospitalises','cas_confirmes','reanimation']
 result=["deces"]
 
@@ -40,7 +46,7 @@ y = y.values.reshape(size,)
 X_result=covid[size:][subject]
 #X_result=X_result.values.reshape(348-size,1)
 y_result=covid[size:][result]
-y_result = y_result.values.reshape(348-size,)
+y_result = y_result.values.reshape(full_size-size,)
 
 params={
     'mlpregressor__max_iter': [10000],
@@ -58,8 +64,14 @@ print(grid.score(X,y))
 print(grid.score(X_result,y_result))
 print(grid.best_params_)
 
+
+
+timeshift_day(covid['deces'],7)
+covid=covid[:full_size-7]
+
+
 plt.figure()
 plt.plot(covid.index,covid['deces'])
-plt.plot(covid.index[size:],grid.predict(X_result))
+plt.plot(covid.index[size-7:],grid.predict(X_result))
 plt.plot(covid.index[:size],grid.predict(X))
 plt.show()
