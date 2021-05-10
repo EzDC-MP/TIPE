90d9e158f4409d612b5fec37ad79a90d773b5989
EnzoDC <ezdecarvalho@gmail.com>
2021-05-10 17:41:31 +0200

    Màj du tex + du code sur le multiregresseur sur la nouvelle base de donnée

--- covid_deces_multiregresseur.py
+++ covid_deces_multiregresseur.py
@@ -15,22 +15,25 @@ from sklearn.model_selection import TimeSeriesSplit
 ##############################################################################
 tscv = TimeSeriesSplit(n_splits=15) #Decoupage pour le CV adapté aux series temporelles.
 scorer = make_scorer(mean_squared_error, greater_is_better=False) #https://scikit-learn.org/stable/modules/model_evaluation.html#mean-squared-error
-model = make_pipeline(StandardScaler(), RegressorChain(SVR(), order=[0,2,1]))
+model = make_pipeline(StandardScaler(), RegressorChain(SVR(), order=[2,1,0]))
 params = {'regressorchain__base_estimator__C': [10**i for i in range(0,8)],
-          'regressorchain__base_estimator__epsilon': [0.001,0.1,0.01] }
-grid = GridSearchCV(model, params, cv = tscv)
+          'regressorchain__base_estimator__epsilon': [0.001,0.1,0.01],
+          'regressorchain__base_estimator__gamma' : ['scale', 'auto'],
+          'regressorchain__base_estimator__kernel': ['rbf']}
+grid = GridSearchCV(model, params, cv = tscv, verbose = 1)
 
 ##############################################################################
-cdata = pd.read_csv('covid_numbers.csv',index_col='date',parse_dates=True)
+#cdata = pd.read_csv('covid_numbers.csv',index_col='date',parse_dates=True) nouvelle base de données màj (10-05-21)
+cdata = pd.read_csv('covid_numbers-10-05-21.csv',index_col='date',parse_dates=True)
 cdata = cdata[cdata['granularite']=='pays']
 for i in cdata:
-    if not (cdata[i].name in ["deces", "reanimation", "cas_confirmes"]):
+    if not (cdata[i].name in ["deces", "reanimation", "hospitalises"]):
         cdata.drop([i], axis=1, inplace = True)
         
 cdata = cdata.dropna(axis=0)
 
 ##############################################################################
-date = '2020-10-25'
+date = '2020-04-30'
 
 Y = cdata['2020-03-01':date]
 x = pd.to_datetime(Y.index)
@@ -44,7 +47,7 @@ x_futur = x_futur.values.reshape(len(x_futur),1)
 
 cdata['deces'].plot(label="deces reels")
 cdata['reanimation'].plot(label="reanimation reels")
-cdata['cas_confirmes'].plot(label="cas_confirmes reels")
+cdata['hospitalises'].plot(label="hospitalisation")
 
 plt.yscale('log')
 plt.legend()
